79e7db79e38b94364147c122567e3166802853ba
malinko <malin88able@gmail.com>
2021-01-22 21:18:20 +0200

    Third commit of my student project

--- viceversa/viceversa/views.py
+++ viceversa/viceversa/views.py
@@ -8,4 +8,7 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    words = user_text.lower().split()
+    words_count = len(words)
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text,
+                                            'words_count': words_count})
